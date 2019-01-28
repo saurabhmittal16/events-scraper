d16a76a09273223a00bb3883a62978b9c7cf6923
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-01-29 00:37:57 +0530

    Change to use async await

--- src/index.js
+++ src/index.js
@@ -2,29 +2,21 @@ const fs = require('fs')
 const scrape = require('./facebook');
 const single = require('./singleEvent');
 
-scrape().then(
-    data => {
-        fs.writeFile('result.json', JSON.stringify(data, null, 4), (err) => {
-            if (err)
-                console.log('Error writing file')
-            else
-                console.log("Saved result.json")
-        })
-    }
-).then(
-    single().then(
-        res => {
-            fs.writeFile('result.json', JSON.stringify(res, null, 4), (err) => {
-                if (err)
-                    console.log('Error writing file')
-                else
-                    console.log("Updated result.json")
-            })
-        }
-    ).catch(
-        err => console.log(err)
-    )
-)
-.catch(
-    err => console.log(err)
-);
+const init = async () => {
+    const data = await scrape();
+    fs.writeFileSync('result.json', JSON.stringify(data, null, 4), (err) => {
+        if (err)
+            console.log('Error writing file')
+        else
+            console.log("Saved result.json")
+    });
+    const updatedData = await single();
+    fs.writeFileSync('result.json', JSON.stringify(updatedData, null, 4), (err) => {
+        if (err)
+            console.log('Error writing file')
+        else
+            console.log("Updated result.json")
+    });
+}
+
+init();
