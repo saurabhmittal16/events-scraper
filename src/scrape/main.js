71f5cf0f341199183937fc3c0f827e65a084c22c
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-03-20 16:31:26 +0530

    use regexp to extract event link and id

--- src/scrape/main.js
+++ src/scrape/main.js
@@ -6,6 +6,9 @@ const pages = Object.keys(require('../../pages'));
 
 const getEventURL = (pageID) => `https://facebook.com/pg/${pageID}/events`;
 
+const linkRegex = /https:\/\/www.facebook.com\/events\/[0-9]{15,}\//gm;
+const eventIDRegex = /[0-9]{15,}/;
+
 async function run() {
     const browser = await puppeteer.launch({
         headless: false,
@@ -31,7 +34,7 @@ async function run() {
                 for(let i=1; i<events.length; i++) {
                     try {
                         let event = events[i].firstElementChild.firstElementChild.firstElementChild;
-                        let date = event.childNodes[0].firstElementChild;
+                        // let date = event.childNodes[0].firstElementChild;
                         // let finalDate = date.childNodes[1].innerText + " " + date.childNodes[0].innerText;
                 
                         let details = event.childNodes[1].firstElementChild;
@@ -39,20 +42,21 @@ async function run() {
                         let link = title.href; 
                         let name = title.firstElementChild.innerText;
                         // let realDate = details.childNodes[1].childNodes[0].innerText;
-                        let guests = details.childNodes[1].childNodes[2].textContent;
+                        // let guests = details.childNodes[1].childNodes[2].textContent;
                 
+                        link = link.match(linkRegex)[0];
                         let loc = event.childNodes[2].firstElementChild;
                         let exact = loc.childNodes[0].firstElementChild.innerText;
                         let city = loc.childNodes[1].innerText;
-                        let eventID = link.slice(32, 47);
+                        let eventID = link.match(eventIDRegex)[0];
             
                         es.push({
                             name,
                             location: exact,
                             city,
                             eventID,
-                            link: link.slice(0, 48)
-                        })
+                            link: link
+                        });
                     } catch (err) {
                         console.log(err);
                     }
